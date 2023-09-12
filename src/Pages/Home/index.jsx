a908c3d27770c80797bbfd7c08e6f873f8e8282d
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-12 10:13:22 -0600

    Abriendo el detalle de cada producto

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
+import ProductDetail from "../../Components/ProductDetail";
 
 function Home() {
   const [items, setItems] = useState(null);
@@ -19,6 +20,7 @@ function Home() {
           <Card key={item.id} data={item} />
         ))}
       </div>
+      <ProductDetail />
     </Layout>
   );
 }
